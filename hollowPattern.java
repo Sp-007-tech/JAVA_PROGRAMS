0c23bf31b14a68c03f61b5e2da78e8305d2c8b4f
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2022-11-18 17:00:36 +0530

    .

---
+++ hollowPattern.java
@@ -0,0 +1,18 @@
+public class hollowPattern{
+    public static void main(String args[]){
+        int n = 10;
+        int m = 15;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(i==1 || j==1 || i==n || j==m){
+                    System.out.print(" * ");
+                }
+                else{
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
