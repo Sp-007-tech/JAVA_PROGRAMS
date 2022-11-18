3a4ee095b9f37e6da313af52f9491bae0c9a9a7f
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2022-11-18 17:06:19 +0530

    Add files via upload

---
+++ invertedHalfPyramid.java
@@ -0,0 +1,10 @@
+public class invertedHalfPyramid {
+    static public void main(String args[]) {
+        for (int i = 0; i <= 5; i++) {
+            for (int j = i; j <= 4; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
