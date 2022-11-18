0cd93424bb5fa96c9fa3b0f381af89d8181705da
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2022-11-18 17:09:10 +0530

    .

---
+++ oneToTen.java
@@ -0,0 +1,14 @@
+public class oneToTen {
+    static public void main(String args[]) {
+
+        int n = 10;
+        int numb = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(numb + " ");
+            }
+            System.out.println();
+        }
+    }
+}
