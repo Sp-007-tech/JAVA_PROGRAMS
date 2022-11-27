d9ec959e27bf8c43bae925f46df23fd80cce7c06
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2022-11-27 10:24:35 +0530

    Uploaded !

---
+++ userInput.java
@@ -0,0 +1,42 @@
+
+
+// Sum of the numbers..
+
+// import java.util.*;
+
+// public class userInput {
+
+//     static public void main(String args[]) {
+
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("enter your number");
+
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int c = sc.nextInt();
+
+//         int sum = a + b + c;
+
+//         System.out.println("The sum is : " + sum);
+
+//     }
+// }
+
+
+
+import java.util.Scanner;
+
+public class userInput {
+    static public void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a name");
+
+        String str = sc.nextLine();
+
+        System.out.println("entered name is " + str);
+
+    }
+}
