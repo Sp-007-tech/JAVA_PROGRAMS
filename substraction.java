447e34d16d868fb2656a1fda06aa43ba3dc0fa32
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2022-11-29 08:09:19 +0530

    uploaded !

---
+++ substraction.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class substraction {
+
+    static public void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number : ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number : ");
+        int b = sc.nextInt();
+
+        int c = a - b;
+
+        System.out.println("The substraction of the number is : " + c);
+    }
+
+}
