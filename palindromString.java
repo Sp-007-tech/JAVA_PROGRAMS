8bc4b71b1605e8ea9e3cc58b18c26144742cfd6b
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2022-11-30 17:11:10 +0530

    uploaded !

---
+++ palindromString.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class palindromString {
+    static public void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a String : ");
+
+        String str = sc.nextLine();
+
+        String rev = " "; 
+
+        String temp = " ";
+
+
+        for(int i= str.length()-1;i>=0;i--){
+            rev = rev + str.charAt(i);
+        }
+
+        
+        if(temp.equals(rev)){
+            System.out.print(rev + " is a palindrom string.");            
+        }else{
+            System.out.print(rev + " is not a palindrom string.");
+        }
+    }
+}
