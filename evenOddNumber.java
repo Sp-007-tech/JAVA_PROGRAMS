e4f489e15dcd92f617342a8aeb568380cec6b58e
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2022-12-05 00:10:11 +0530

    uploaded !

---
+++ evenOddNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class evenOddNumber {
+    static public void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number : ");
+
+        int a =sc.nextInt();
+
+        if(a%2 == 0){
+            System.out.println("Its a even number");
+        }else{
+            System.out.println("Its not a even number");
+        }
+    }
+}
