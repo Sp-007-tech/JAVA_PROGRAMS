32c426a30c7e83b478e2e29693fb2208d593dbae
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2022-12-05 00:10:35 +0530

    uploaded !

---
+++ fibonacciSeries.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class fibonacciSeries {
+
+    static void fab(int n) {
+        int n1 = 0, n2 = 1, n3;
+
+        System.out.print(n1 + " " + n2);
+
+        for (int i = 1; i <= n; i++) {
+            n3 = n1 + n2;
+
+            System.out.print(" " + n3);
+
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+
+    static public void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number : ");
+
+        int n = sc.nextInt();
+
+        fab(n);
+    }
+
+}
