a603ea2f1d79a2fa2b72f419b82bc526724fe09a
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2022-12-05 00:11:12 +0530

    uploaded !

---
+++ positiveNegative.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class positiveNegative {
+    static public void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+
+        int n = sc.nextInt();
+
+        if(n>0){
+            System.out.print("It is a positive number. ");
+        }
+        else{
+            System.out.println("Its negative number.");
+        }
+    }
+    
+}
