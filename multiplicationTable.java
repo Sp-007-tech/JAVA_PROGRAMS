20edd3bad480ef1dffa445840303e2f6980e0443
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2022-12-06 00:26:38 +0530

    uploaded !

---
+++ multiplicationTable.java
@@ -0,0 +1,12 @@
+public class multiplicationTable {
+    static public void main(String args[]){
+
+        for(int i=1;i<=10;i++){
+            for(int j=1;j<=10;j++){
+            System.out.print(i  * j );
+            }
+            System.out.println( " ");
+        }
+    
+    }    
+}
