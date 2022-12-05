6b8b771654379f73e3c093794c63db4ec006d19b
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2022-12-06 00:27:16 +0530

    uploaded !

---
+++ overloading.java
@@ -0,0 +1,20 @@
+public class overloading{
+
+    static int sum(int a, int b){
+        return a+b;
+    }
+
+    static int sum(int a, int b, int c){
+        return ((a+b)+c);
+    }
+
+    static float sum(float a , float b){
+        return a+b;
+    }
+
+    static public void main(String args[]){
+        System.out.println(sum(9,6));
+        System.out.println(sum(6,9,6));
+        System.out.println(sum(69.6f,69.6f));
+    }
+}
